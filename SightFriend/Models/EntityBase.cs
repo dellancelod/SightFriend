71d5eb49846471dc143f7957394bce8964b2e3a7
dellancelod <danil.svetaylo@gmail.com>
2023-04-26 09:09:47 +0300

    Extract EntityBase model

--- SightFriend/Models/EntityBase.cs
+++ SightFriend/Models/EntityBase.cs
@@ -1,6 +1,13 @@
-﻿namespace SightFriend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SightFriend.Models
 {
-    public class EntityBase
+    public abstract class EntityBase
     {
+        protected EntityBase() => DateAdded = DateTime.UtcNow;
+        [Required]
+        public virtual Guid Id { get; set; }
+        [DataType(DataType.Time)]
+        public virtual DateTime DateAdded { get; set; }
     }
 }
