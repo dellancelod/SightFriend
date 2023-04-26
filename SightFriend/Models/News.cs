6e63133b88af13888de2b10183bb3d0d1d101deb
dellancelod <danil.svetaylo@gmail.com>
2023-04-26 09:11:56 +0300

    Update News model

--- SightFriend/Models/News.cs
+++ SightFriend/Models/News.cs
@@ -2,20 +2,13 @@
 
 namespace SightFriend.Models
 {
-    public class News
+    public class News : EntityBase
     {
-        private News() => DateAdded = DateTime.UtcNow;
-
-        [Required]
-        public Guid Id { get; set; }
-        [Display(Name = "Заголовок")]
         public string Title { get; set; }
         [Display(Name = "Опис")]
         public string Subitle { get; set; }
         [Display(Name = "Зміст")]
         public string Text { get; set; }
-        [DataType(DataType.Time)]
-        public DateTime DateAdded { get; set; }
 
     }
 }
