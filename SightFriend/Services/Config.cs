e828c22d9f1ec80c7d6cb2d855e0737bf6f2d003
dellancelod <danil.svetaylo@gmail.com>
2023-04-26 10:22:24 +0300

    Remove Config class

--- SightFriend/Services/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SightFriend.Services
-{
-    public class Config
-    {
-        public static string ConnectionString { get; set; }
-        public static string CompanyName { get; set; } 
-    }
-}
