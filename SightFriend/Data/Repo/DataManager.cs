09ae8220e42a4d2e3c5da37dedc07ff4ac447dec
dellancelod <danil.svetaylo@gmail.com>
2023-04-26 09:23:44 +0300

    Add DataManager helper class

---
+++ SightFriend/Data/Repo/DataManager.cs
@@ -0,0 +1,12 @@
+﻿namespace SightFriend.Data.Repo
+{
+    public class DataManager
+    {
+        public INewsItemsRepository NewsItems { get; set; }
+
+        public DataManager(INewsItemsRepository newsItemsRepository)
+        {
+            NewsItems = newsItemsRepository;
+        }
+    }
+}
