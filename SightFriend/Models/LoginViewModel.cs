c375f5fb7d83c9521a235a5111574be929f2d841
dellancelod <danil.svetaylo@gmail.com>
2023-04-26 10:59:38 +0300

    Create LoginViewModel

---
+++ SightFriend/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SightFriend.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Номер телефону")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [UIHint("Введіть пароль")]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+    }
+}
