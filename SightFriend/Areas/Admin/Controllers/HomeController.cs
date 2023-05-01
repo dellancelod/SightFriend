a42f38041d945bcf3ffefe840d2f3fe61b6b6d81
dellancelod <danil.svetaylo@gmail.com>
2023-05-01 14:38:37 +0300

    Inject DataManager in HomeController

--- SightFriend/Areas/Admin/Controllers/HomeController.cs
+++ SightFriend/Areas/Admin/Controllers/HomeController.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SightFriend.Data.Repo;
 
 namespace SightFriend.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+        public HomeController(DataManager dataManager) 
+        {
+            this.dataManager = dataManager;
+        }
         public ActionResult Index()
         {
-            return View();
+            return View(dataManager.NewsItems.GetNews());
         }
     }
 }
