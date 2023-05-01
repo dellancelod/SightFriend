1fcecd5f5b3f1097914cf928a3e864dbfc7299d1
dellancelod <danil.svetaylo@gmail.com>
2023-05-01 16:36:56 +0300

    Change routes order

--- SightFriend/Program.cs
+++ SightFriend/Program.cs
@@ -73,14 +73,14 @@ app.UseCookiePolicy();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseEndpoints((endpoints) => { 
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-    );
+app.UseEndpoints((endpoints) => {
     endpoints.MapControllerRoute(
         name: "admin",
         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
     );
 }
 );
