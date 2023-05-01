ba7895d2dda4266bbe89c4e0f6569b55cbd27c23
dellancelod <danil.svetaylo@gmail.com>
2023-05-01 10:55:58 +0300

    Create HomeController in Admin Area

---
+++ SightFriend/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SightFriend.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
