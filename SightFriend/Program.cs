1a85d976c47d5fec323a640f9df484acffc16ed4
dellancelod <danil.svetaylo@gmail.com>
2023-11-29 22:22:34 +0200

    Add animation to login form

--- SightFriend/Program.cs
+++ SightFriend/Program.cs
@@ -53,6 +53,8 @@ builder.Services.AddControllersWithViews(x =>
     x.Conventions.Add(new AdminAreaAuthorization("Admin", "AdminArea"));
 });
 
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
