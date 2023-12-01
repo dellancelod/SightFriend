88b6996c62990b03f8cac5ec94c23d7e839f270a
dellancelod <danil.svetaylo@gmail.com>
2023-12-01 17:02:45 +0200

    Add TextFields edit view

---
+++ SightFriend/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SightFriend.Data;
+using SightFriend.Models;
+
+namespace SightFriend.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TextFieldsController : Controller
+    {
+        private readonly DataManager dataManager;
+        public TextFieldsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Edit(string codeWord)
+        {
+            var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TextField model)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.TextFields.SaveTextField(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).Replace("Controller", string.Empty));
+            }
+            return View();
+        }
+    }
+}
